773ad3ab3a6076ae1e41ad289c17e95b5e60dc59
darshanbib <darshan@gmail.com>
2011-12-12 10:27:05 -0500

    gmail ad block update

--- gmail-ad-block/gmail.adblock.user.js
+++ gmail-ad-block/gmail.adblock.user.js
@@ -1,13 +1,12 @@
 // ==UserScript==
 // @name          Gmail AdBlock
+// @namespace     https://github.com/darshanbib/greasemonkey-scripts
 // @description   Removes ads from Gmail and Google Apps Mail
 // @include       https://*.google.com/*
+// @version       0.1
 // ==/UserScript==
 
 var ads = document.getElementsByClassName('mq');
 for (i=0; i<ads.length; i++) {
-  ads[i].style.height = '1px';
-  ads[i].style.visibility = 'none';
-  ads[i].style.margin = '0px';  
-  ads[i].style.padding = '0px';
+    ads[i].style.display = 'none';
 }
